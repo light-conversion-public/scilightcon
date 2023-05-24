3da4a8520480cacf956570503e1a15bdce130ddf
Vytautas Butkus <vytautas.butkus@lightcon.com>
2023-05-24 16:47:50 +0300

    Added toolbox_materials.pkl and updating script

---
+++ build_tools/update_materials_db.py
@@ -0,0 +1,18 @@
+import pymongo
+import pickle
+import os
+# set $env:CONN_STR to read-only connection string to toolbox-mongo.db
+
+CONN_STR = os.environ['CONN_STR']
+
+client = pymongo.MongoClient(CONN_STR)
+
+db = client['Toolbox']
+collection = db['Materials']
+
+cursor = collection.find({})
+
+materials = {element['Key'] : element['Value'] for element in cursor}
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../scilightcon/datasets/data/toolbox_materials.pkl'), 'wb') as f:
+    pickle.dump(materials, f)
