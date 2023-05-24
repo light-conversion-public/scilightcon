4b1db718971fcca6a32a3b325e67908cbe6ca68d
nurbonaite <neda.urbonaite@lightcon.com>
2023-05-24 10:17:58 +0300

    MSD-815 [utils] Multiply several [X,Y] datasets with interpolation w.r.t. the first input

--- scilightcon/__init__.py
+++ scilightcon/__init__.py
@@ -2,8 +2,10 @@ __version__ = "0.0.4"
 
 from . import foo
 from . import plot
+from . import utils
 
 __all__ = [
     "foo",
-    "plot"
+    "plot",
+    "utils"
 ]
---
+++ scilightcon/utils/__init__.py
@@ -0,0 +1,8 @@
+
+"""Module for loading datasets"""
+
+from ._interpolate_and_multiply import interpolate_and_multiply
+
+__all__ = [
+    "interpolate_and_multiply"
+]
