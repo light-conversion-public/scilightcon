79a377d652025d5f8f81a6f84dc0af6ee6228cab
Vytautas Butkus <vytautas.butkus@lightcon.com>
2023-06-05 13:29:26 +0300

    modified test

--- tests/test_utils_load_s2s_data.py
+++ tests/test_utils_load_s2s_data.py
@@ -5,8 +5,7 @@ from importlib import resources
 
 def test_utils_load_s2s_data():
 
-    filepath = _get_path(scilightcon.datasets.DATA_MODULE, 'Shot-to-shot_LAB4 PHAROS_25.0kHz_1030nm_InGaAs_20210917_1337.s2s')
-
-    s2s_data = load_s2s_data(filepath)
+    with _get_path(scilightcon.datasets.DATA_MODULE, 'Shot-to-shot_LAB4 PHAROS_25.0kHz_1030nm_InGaAs_20210917_1337.s2s') as filepath:
+        s2s_data = load_s2s_data(filepath)
 
     assert(len(s2s_data.outliers) == 5)
