8682f730fec8bbc9feef67831be4e9dbe58dd1d3
Vytautas Butkus <vytautas.butkus@lightcon.com>
2023-06-05 13:18:27 +0300

    updated test

--- tests/test_utils_load_s2s_data.py
+++ tests/test_utils_load_s2s_data.py
@@ -1,10 +1,11 @@
 import scilightcon
 from scilightcon.utils import load_s2s_data
+from scilightcon.utils._fixes import _get_path
 from importlib import resources
 
 def test_utils_load_s2s_data():
 
-    filepath = resources.files(scilightcon.datasets.DATA_MODULE).joinpath('Shot-to-shot_LAB4 PHAROS_25.0kHz_1030nm_InGaAs_20210917_1337.s2s')
+    filepath = _get_path(scilightcon.datasets.DATA_MODULE, 'Shot-to-shot_LAB4 PHAROS_25.0kHz_1030nm_InGaAs_20210917_1337.s2s')
 
     s2s_data = load_s2s_data(filepath)
 
