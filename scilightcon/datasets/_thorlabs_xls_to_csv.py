7bcdf7c8f5c2e94a70092d2f3a74c45a9776e683
Lukas Kontenis <lukas.kontenis@lightcon.com>
2025-06-11 11:19:46 +0300

    New function thorlabs_xls_to_csv with examples

--- scilightcon/datasets/_thorlabs_xls_to_csv.py
+++ scilightcon/datasets/_thorlabs_xls_to_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 
-def thorlabs_xls_to_csv(file_name):
+def thorlabs_xls_to_csv(file_name, mode='trans'):
     """
     XLS to CSV converter for Thorlabs filter and mirror transmission and
     reflection data.
@@ -12,10 +12,22 @@ def thorlabs_xls_to_csv(file_name):
         >>> from scilightcon.datasets import thorlabs_xls_to_csv # doctest: +SKIP
         >>> wavl, trans = thorlabs_xls_to_csv('DMLP505.xls') # doctest: +SKIP
     """
-    data = pd.read_excel(file_name, skiprows=1, usecols='C:E')
+    data = pd.read_excel(file_name, usecols='C:E')
 
     wavl = np.array(data.iloc[:, 0], dtype='int')
-    trans = np.array(data.iloc[:, 1])
+
+    if mode == 'trans':
+        ydata = np.array(data.iloc[:, 1])
+        preffix = 'transmission'
+        yunits = 'Transmission (%)'
+        if not 'transmi' in data.columns[1].lower():
+            raise ValueError("Second column is not transmission")
+    elif mode == 'refl':
+        ydata = np.array(data.iloc[:, 2])
+        preffix = 'reflection'
+        yunits = 'Reflectance (%)'
+        if not 'reflec' in data.columns[2].lower():
+            raise ValueError("Second column is not reflectance")
 
     if not 'wavelength' in data.columns[0].lower():
         raise ValueError("First column is not wavelength")
@@ -23,11 +35,8 @@ def thorlabs_xls_to_csv(file_name):
     if not 'nm' in data.columns[0].lower():
         raise ValueError("Wavelength is not in nm")
 
-    if not 'transmi' in data.columns[1].lower():
-        raise ValueError("Second column is not transmission")
-
     if not '%' in data.columns[1].lower():
-        raise ValueError("Transmission is not in %")
+        raise ValueError("Data is not in %")
 
     if np.min(wavl < 0):
         raise ValueError("Wavelength < 0")
@@ -35,18 +44,17 @@ def thorlabs_xls_to_csv(file_name):
     if np.max(wavl < 100):
         raise ValueError("Wavelength is probably not in nm")
 
-    if np.min(trans) < 0:
-        raise ValueError("Transmission < 0")
+    if np.min(ydata) < -1:
+        raise ValueError("Y data (transmission or reflectance) < 0%")
 
-    if np.max(trans) < 2:
-        raise ValueError("Transmission is probably not in %")
+    if np.max(ydata) < 2:
+        raise ValueError("Y data (transmission or reflectance) is probably not in %")
 
-    np.savetxt(f"transmission_THORLABS_{Path(file_name).stem}.csv",
-            np.transpose([wavl, trans]),
-            header="Wavelength  (nm), Transmission (%)",
+
+    np.savetxt(f"{preffix}_THORLABS_{Path(file_name).stem}.csv",
+            np.transpose([wavl, ydata]),
+            header=f"Wavelength  (nm), {yunits}",
             fmt="%.6f",
             delimiter=', ')
 
-    return wavl, trans
-
-
+    return wavl, ydata
