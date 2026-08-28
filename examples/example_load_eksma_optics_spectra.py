b8b299f597ced47ea258532580dbed6ea35649dc
Lukas Kontenis <lukas.kontenis@lightcon.com>
2026-08-28 14:02:10 +0300

    Fixed Edmund Optics and Eksma examples

---
+++ examples/example_load_eksma_optics_spectra.py
@@ -0,0 +1,15 @@
+from scilightcon.datasets import load_eksma_spectrum
+import matplotlib.pyplot as plt
+
+plt.figure()
+
+for material in ['Au', 'Ag', 'Al']:
+    data, headers = load_eksma_spectrum(material)
+    plt.plot(data[:,0], data[:,1], label = material)
+
+plt.xlabel(headers[0])
+plt.ylabel(headers[1])
+plt.legend()
+plt.grid(True)
+
+plt.show()
